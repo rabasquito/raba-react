60c2bec2ea9ea9e185f11ef1c10db85b18a32686
Rodrigo <rabasco.ro@gmail.com>
2021-11-08 23:37:41 -0300

    Agregando componente NavBar al proyecto

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+
+
+export const NavBar = () => {
+
+    return (
+        <header className="header">
+            <h1>LOGO</h1>
+
+            <nav>
+                <ul>
+                    <li>Pag1</li>
+                    <li>Pag2</li>
+                    <li>Pag3</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+// export default NavBar
