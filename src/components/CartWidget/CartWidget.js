b6e4089da8cd5a3710cf4adc453de82ae94d47e5
Rodrigo <rabasco.ro@gmail.com>
2021-12-09 18:15:28 -0300

    hidden al cartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,16 +1,19 @@
 import React, { useContext } from 'react'
 import {FaShoppingCart} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
 import './CartWidget.scss'
 
 export const CartWidget = () => {
     
-    const {totalCantidad} = useContext(CartContext)
+    const {totalCantidad, carrito} = useContext(CartContext)
 
     return (
-        <div>
+        <div className={carrito.length === 0 ? 'hidden' : ''}>
+            <Link to="cart">
             <FaShoppingCart className="cartWidget" />
             <span>{totalCantidad()}</span>
+            </Link>
         </div>
     )
 }
